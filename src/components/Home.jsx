0f8fff5da4d8f954a61e8f1251a607c6ba70fa0e
adityaraj790391@gmail.com <adityaraj790391@gmail.com>
2026-04-29 01:26:03 +0530

    first commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,11 +8,13 @@ const Home = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div>
+      <div className='overflow-y-scroll overflow-x-hidden h-[calc(100vh-2rem)]'>
+      <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 p-4 mt-33'>
         {data.map((item) => {
           if (item.type !== "video") return false;
           return <Video key={item.Id} video={item?.video} />;
         })}
+        </div>
       </div>
     </div>
   );
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Time from '../loader/Time'
+import { SiTicktick } from "react-icons/si";
 
 const Video = ({video}) => {
   return (
     <div className=''>
         <Link to = {`/video/${video?.id}`}>
-           <div className='flex'> 
-            <div className='h-60 w-100 rounded-2xl duration-300 overflow-hidden hover:rounded-none'>
-                <img className='w-full h-full object-cover' src={video?.thumbnails?.[0]?.url} alt=""/>
+           <div> 
+            <div className='h-60 relative rounded-xl duration-300 overflow-hidden hover:rounded-none'>
+                <img className='h-full w-full object-cover' src={video?.thumbnails?.[0]?.url} alt=""/>
                 {video?.lengthSeconds && <Time time = {video?.lengthSeconds} />}
+            </div>
+            <div className='flex space-x-3 mt-2'>
+                <div className='flex items-start'>
+                    <div className='h-9 w-9 border rounded-full overflow-hidden'>
+                        <img className='h-full w-full rounded-full' src={video?.author?.avatar[0].url} alt=''></img>
+                    </div>
+                </div>
+                <div>
+                    <span className='text-[16px] font-semibold line-clamp-2'> {video?.title} </span>
+                    <span className='flex gap-1 items-center text-sm font-semibold text-gray-600'>
+                        {video?.author?.title}
+                        {video?.author?.badges[0]?.type === 'VERIFIED_CHANNEL' && <SiTicktick className='text-sm bg-gray-200 rounded-full'/>}
+                    </span>
+                    <div>
+                        <p>{video?.stats?.views}</p>
+                        <p></p>
+                    </div>
+                </div>
             </div>
            </div>
         </Link>
